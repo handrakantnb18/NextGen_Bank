d213323f5a4857ad6f3574b80ca8f1d4b99c4501
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-22 22:50:20 +0530

    EmailAlreadyExistsException extends RuntimeException

---
+++ GlobalTrust/src/main/java/com/shivdattTrust/bank/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.shivdattTrust.bank.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+	
+	public EmailAlreadyExistsException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
