0a84d850ef687761dda54d9cbc0bf1959abd3cc1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-22 22:48:29 +0530

    AuthController private final AuthService authService ResponseEntity<AuthResponse>

---
+++ GlobalTrust/src/main/java/com/shivdattTrust/bank/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.shivdattTrust.bank.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shivdattTrust.bank.dto.AuthResponse;
+import com.shivdattTrust.bank.dto.LoginRequest;
+import com.shivdattTrust.bank.dto.RegisterRequest;
+import com.shivdattTrust.bank.service.AuthService;
+
+import ch.qos.logback.core.net.LoginAuthenticator;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final AuthService authService;
+	
+	@PostMapping("/register")
+	public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request)
+	{
+		AuthResponse response = authService.register(request);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request)
+	{
+		AuthResponse response = authService.login(request);
+		return ResponseEntity.ok(response);
+	}
+	
+}
