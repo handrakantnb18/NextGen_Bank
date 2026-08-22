d82ced59e14b2fc79b4780b51d971ad50ed869a0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-22 22:47:39 +0530

    SecurityConfig SecurityFilterChain

---
+++ GlobalTrust/src/main/java/com/shivdattTrust/bank/config/SecurityConfig.java
@@ -0,0 +1,92 @@
+package com.shivdattTrust.bank.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import com.shivdattTrust.bank.security.JwtAuthenticationFilter;
+import com.shivdattTrust.bank.service.CustomUserDetailsService;
+
+public class SecurityConfig {
+
+	private final UserDetailsService userDetailsService;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	private static final String[] PUBLIC_ENDPOINTS = {
+			"/api/auth/register",
+			"/api/auth/login"
+			
+	};
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
+		http.csrf(csrf -> csrf.disable())
+		.cors(cors -> 
+		cors.configurationSource(corsConfigurationSource()))
+		.sessionManagement(session -> 
+		session.sessionCreationPlicy(SessionCreationPolicy.STATELESS))
+		.authorizationHttpRequests(auth -> auth
+				.requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+				.requestMatchers("/api/admin/**")
+				.hasRole("ADMIN")
+				.anyRequest().authenticated())
+		.authenticationProvider(authenticationProvider())
+		.addFilterBefore(jwtAuthenticationFilter,
+				UsernamePasswordAuthenticationFilter.class);
+		
+		return http.build();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider()
+	{
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(PasswordEncoder());
+		return provider;
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+	throws Exception {
+		return config.getAuthenticationManager();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsconfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:5173"));
+		
+		config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+		
+		config.setAllowedMethods(List.of("Authorization","Content-Type"));
+		config.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		
+		source.registerCorsConfiguration("/**", config);
+		
+		return source;
+		
+		
+	}
+}
