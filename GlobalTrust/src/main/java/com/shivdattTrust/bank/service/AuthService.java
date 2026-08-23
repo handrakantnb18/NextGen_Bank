2fb671ed14f16c3ec88eaad187a794d72753dd61
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-23 21:50:48 +0530

    AuthService transaction.Transactional UserRepository AuthResponse

--- GlobalTrust/src/main/java/com/shivdattTrust/bank/service/AuthService.java
+++ GlobalTrust/src/main/java/com/shivdattTrust/bank/service/AuthService.java
@@ -3,7 +3,7 @@ package com.shivdattTrust.bank.service;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,57 +11,61 @@ import com.shivdattTrust.bank.dto.AuthResponse;
 import com.shivdattTrust.bank.dto.LoginRequest;
 import com.shivdattTrust.bank.dto.RegisterRequest;
 import com.shivdattTrust.bank.entity.Role;
+import com.shivdattTrust.bank.entity.User;
 import com.shivdattTrust.bank.exception.EmailAlreadyExistsException;
 import com.shivdattTrust.bank.exception.InvalidCredentialsException;
 import com.shivdattTrust.bank.repository.UserRepository;
 import com.shivdattTrust.bank.security.JwtUtil;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
-	private final UserRepository userRepository;
-	
-	private final PasswordEncoder passwordEncoder;
-	
-	private final AuthenticationManager authenticationManager;
-	
-	private JwtUtil jwtUtil;
-	
-	
-	public AuthResponse register(RegisterRequest request)
-	{
-		if(userRepository.existsByEmail(request.getEmail())) {
-			throw new EmailAlreadyExistsException(request.getEmail());
-		}
-		
-		User user = User.builder()
-				.name(request.getName())
-				.email(request.getEmail())
-				.phone(request.getPhone())
-				.passwordHash(passwordEncoder.encode(request.getPassword()))
-				.role(Role.CUSTOMER)
-				.active(true)
-				.build();
-		
-		User saved = userRepository.save(user);
-		String token = jwtUtil.generateToken(saved);
-		
-		return AuthResponse.of(token, saved.getId(), saved.getName(), saved.getEmail(), saved.getRole().name());
-		
-		public AuthResponse login(LoginRequest request)
-		{
-			try {
-				authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-			}
-			catch(BadCredentialsException ex) {
-				throw new InvalidCredentialsException();
-			}
-			
-			User user = 
-		}
-		
-	}
+	 private final UserRepository userRepository;
+	    private final PasswordEncoder passwordEncoder;
+	    private final AuthenticationManager authenticationManager;
+	    
+	    private final JwtUtil jwtUtil;
+
+	    @Transactional
+	    public AuthResponse register(RegisterRequest request) {
+	        if (userRepository.existsByEmail(request.getEmail())) {
+	            throw new EmailAlreadyExistsException(request.getEmail());
+	        }
+
+	        User user = User.builder()
+	                .name(request.getName())
+	                .email(request.getEmail())
+	                .phone(request.getPhone())
+	                .passwordHash(passwordEncoder.encode(request.getPassword()))
+	                .role(Role.CUSTOMER)
+	                .active(true)
+	                .build();
+
+	        User saved = userRepository.save(user);
+	        String token = jwtUtil.generateToken(saved);
+
+	        return AuthResponse.of(token, saved.getId(), saved.getName(), saved.getEmail(), saved.getRole().name());
+	    }
+
+	    public AuthResponse login(LoginRequest request) {
+	        try {
+	            authenticationManager.authenticate(
+	                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+	            );
+	        } catch (BadCredentialsException ex) {
+	            throw new InvalidCredentialsException();
+	        }
+
+	        User user = userRepository.findByEmail(request.getEmail())
+	        		.orElseThrow(InvalidCredentialsException::new);
+
+	        String token = jwtUtil.generateToken(user);
+
+	        return AuthResponse.of(token, user.getId(), user.getName(), user.getEmail(), user.getRole().name());
+	        
+	    }
 }
