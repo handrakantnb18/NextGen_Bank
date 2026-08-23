5955d4c40551fa30f80e109e2f4768c714509d7d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-23 21:53:11 +0530

    InvalidCredentialsException extends RuntimeException

---
+++ GlobalTrust/src/main/java/com/shivdattTrust/bank/exception/InvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package com.shivdattTrust.bank.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+	public InvalidCredentialsException() {
+		super("Invalied email or password");
+		// TODO Auto-generated constructor stub
+	}
+}
