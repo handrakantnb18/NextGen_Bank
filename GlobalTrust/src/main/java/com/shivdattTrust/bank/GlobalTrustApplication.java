705df7af947a592083fa615a477397e12ad6ccab
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-25 13:20:05 +0530

    GlobalTrustApplication BCryptPasswordEncoder encodedPassword

--- GlobalTrust/src/main/java/com/shivdattTrust/bank/GlobalTrustApplication.java
+++ GlobalTrust/src/main/java/com/shivdattTrust/bank/GlobalTrustApplication.java
@@ -2,12 +2,23 @@ package com.shivdattTrust.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class GlobalTrustApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlobalTrustApplication.class, args);
+		
+		 BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+	        String password = "admin123";
+
+	        String encodedPassword = encoder.encode(password);
+
+	        System.out.println("Password: " + password);
+	        System.out.println("Encoded Password: " + encodedPassword);
+	        
 	}
 
 }
